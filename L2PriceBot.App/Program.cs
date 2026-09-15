544cc66ab925d5af59823971d4b810919414c20c
Nicolas Basilico <nicolasbasilico@gmail.com>
2026-09-15 15:49:43 -0300

    Fix appsettings.json deployment error

--- L2PriceBot.App/Program.cs
+++ L2PriceBot.App/Program.cs
@@ -22,7 +22,7 @@ public class Program
         var host = Host.CreateDefaultBuilder(args)
             .ConfigureAppConfiguration((context, config) =>
             {
-                config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
                 config.AddEnvironmentVariables();
             })
             .ConfigureServices((context, services) =>
